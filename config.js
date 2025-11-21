71ca16ea0d9b53feff1bf1093599dc16add08000
Prince kumar <prince.m2hgamerz@gmail.com>
2025-11-21 20:13:43 +0530

    Update WhatsApp credentials in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
   API_BASE: 'https://wadownloader.amitdas.site/api/',
-  WHATSAPP_INSTANCE_ID: 'YOUR_INSTANCE_ID',
-  WHATSAPP_ACCESS_TOKEN: 'YOUR_ACCESS_TOKEN'
+  WHATSAPP_INSTANCE_ID: '609ACF283XXXX',
+  WHATSAPP_ACCESS_TOKEN: '692078f9005be'
 };
